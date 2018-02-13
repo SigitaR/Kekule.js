0347bcd06ed312136a47c2cd8222238f7917a2e8
Aaron Llanos <allanos@inmotionsoftware.com>
2018-02-13 14:23:41 -0600

    find different way to call child files

--- src/localization/en/kekule.localize.extras.openbabel.en.js
+++ src/localization/en/kekule.localize.extras.openbabel.en.js
@@ -4,21 +4,23 @@
  * @author Partridge Jiang
  */
 
-/** @ignore */
-Kekule.LOCAL_RES = true;
+ module.exports = function (Kekule){
+	/** @ignore */
+	Kekule.LOCAL_RES = true;
 
-Kekule.Localization.setCurrModule('extra.openbabel');
+	Kekule.Localization.setCurrModule('extra.openbabel');
 
-// error messages
-// Object.extend(Kekule.ErrorMsg.en, {
-Kekule.Localization.addResource('en', 'ErrorMsg', {
-	'OpenBabel': {
-		CHEM_NODE_TYPE_NOT_SUITABLE: 'Unsuitable node type',
-		CHEM_CONNECTOR_TYPE_NOT_SUITABLE: 'Unsuitable connector type',
+	// error messages
+	// Object.extend(Kekule.ErrorMsg.en, {
+	Kekule.Localization.addResource('en', 'ErrorMsg', {
+		'OpenBabel': {
+			CHEM_NODE_TYPE_NOT_SUITABLE: 'Unsuitable node type',
+			CHEM_CONNECTOR_TYPE_NOT_SUITABLE: 'Unsuitable connector type',
 
-		ONLY_STR_INPUT_DATA_ALLOWED: 'Input data must be text',
-		ONLY_STR_OUTPUT_DATA_ALLOWED: 'Output data must be text',
+			ONLY_STR_INPUT_DATA_ALLOWED: 'Input data must be text',
+			ONLY_STR_OUTPUT_DATA_ALLOWED: 'Output data must be text',
 
-		FAIL_TO_GENERATE_3D_STRUCTURE: 'Fail to generate 3D structure coordinates'
-	}
-});
+			FAIL_TO_GENERATE_3D_STRUCTURE: 'Fail to generate 3D structure coordinates'
+		}
+	});
+ }
