cd0b825f9caa5d13d58c4398f59cad7c5bf95476
Rachel Agner <rachel.agner@macmillan.com>
2018-02-22 17:00:18 -0600

    kekule is alive! It shows up and you can make bonds and some stuff

--- demos/test.js
+++ demos/test.js
@@ -1,6 +1,75 @@
 const { Kekule } = require("kekule")
+
 const CORRECT_MOL = `{"id":"m1","renderOptions":{"expanded":true,"__type__":"object"},"charge":0,"parity":null,"ctab":{"nodes":[{"__type__":"Kekule.Atom","id":"a1","coord2D":{"x":9.299583333333334,"y":37.51833333333333,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a4","coord2D":{"x":9.299583333333334,"y":36.718333333333334,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a3","coord2D":{"x":8.606763010305784,"y":37.91833333333333,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a2","coord2D":{"x":9.992403656360885,"y":37.91833333333333,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"}],"anchorNodes":[],"connectors":[{"__type__":"Kekule.Bond","id":"b3","parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[0,1]},{"__type__":"Kekule.Bond","id":"b2","parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[0,2]},{"__type__":"Kekule.Bond","id":"b1","parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[0,3]}],"__type__":"Kekule.StructureConnectionTable"},"__type__":"Kekule.Molecule"}`;
 const INCORRECT_MOL = `{"id":"m1","renderOptions":{"expanded":true,"__type__":"object"},"charge":0,"parity":null,"ctab":{"nodes":[{"__type__":"Kekule.Atom","id":"a2","coord2D":{"x":10.499070323027551,"y":37.998333333333335,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a1","coord2D":{"x":9.80625,"y":37.598333333333336,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"}],"anchorNodes":[],"connectors":[{"__type__":"Kekule.Bond","id":"b1","parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[0,1]}],"__type__":"Kekule.StructureConnectionTable"},"__type__":"Kekule.Molecule"}`;
+
+
+var MySingleElectronAction = Kekule.Editor.createComposerIaControllerActionClass(
+     'Kekule.Editor.ActionComposerSetAttachedMarkerIaControllerSingleElectron',
+     'Single Electron',
+     'Add single electron',
+     'AttachedMarkerIaController',
+     'AttachedMarkerIaController-SingleElectron',
+     {
+       'markerClassName': 'Kekule.ChemMarker.UnbondedElectronSet',
+       'targetClassName': 'Kekule.AbstractAtom',
+       'initialPropValues': {'electronCount': 1}
+     },
+     null, null,
+     'singleElectron'
+   );
+
+   // initialize Composer
+  Kekule.X.domReady(function(){
+    var composer = new Kekule.Editor.Composer(document.getElementById('composer'))
+
+     //var composer = Kekule.Widget.getWidgetById('composer');
+     console.log(composer)
+
+     // bind event
+       var BNS = Kekule.ChemWidget.ComponentWidgetNames;
+       console.log('BNS', BNS)
+       composer.setCommonToolButtons([BNS.undo, BNS.redo, BNS.zoomIn, BNS.zoomOut, BNS.objInspector]).setChemToolButtons([
+         BNS.manipulate,
+         BNS.erase,
+         {'name': 'Custom', 'actionClass': Kekule.Editor.ActionOnComposerAdv,
+           'text': 'Create', 'hint': 'My create button', 'id': 'btnMyCreate', 'htmlClass': 'MYBUTTON',
+           'widget': Kekule.Widget.RadioButton,
+           'attached': [
+           BNS.molBondSingle, BNS.molBondDouble, BNS.molBondTriple,  //only show single, double and triple bounds
+           /*  Hide other bond buttons
+
+            BNS.molBondCloser, BNS.molBondWedgeUp, BNS.molBondWedgeDown,
+            BNS.molRepMethane,
+            BNS.molRepFischer1, BNS.molRepFischer2,
+            BNS.molRepSawhorseStaggered, BNS.molRepSawhorseEclipsed
+            */
+           //{'name': BNS.molAtom, 'actionClass': Kekule.Editor.ActionComposerSetAtomController}
+           BNS.molAtom
+         ]},
+         //BNS.molAtom,
+         BNS.molFormula,
+         BNS.molRing,
+         {
+           'name': BNS.molCharge,
+           'attached': [
+             BNS.molChargeClear,
+             BNS.molChargePositive,
+             BNS.molChargeNegative,
+             BNS.molElectronLonePair,
+             {
+               'name': 'singleElectron', 'actionClass': MySingleElectronAction
+             }
+           ]
+         }
+         // BNS.molCharge,
+         // BNS.textImage
+       ]);
+     })
+
+
+
+
 
 function validateKekule (kekuleJSON) {
     let validKekule = false;
