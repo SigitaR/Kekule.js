1da8bc975af977c4e1f69dae22e5c59c3534f3cf
Aaron Llanos <allanos@inmotionsoftware.com>
2018-03-22 15:50:41 -0500

    remove -p flag from build. Rely on Uglify plugin in the webpack config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -43,13 +43,13 @@ module.exports = {
     ws: 'empty'
   },
   plugins: [
-    // new webpack.optimize.UglifyJsPlugin({
-    //   uglifyOptions: {
-    //     mangle: false
-    //   }
-    // })
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin({
-      mangle: false
+      mangle: {
+        except: ['$super']
+      }
     })
   ]
 };
