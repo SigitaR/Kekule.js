c0aaeda55bdbd8ed981df60e3a21612c1faf907f
Aaron Llanos <allanos@inmotionsoftware.com>
2018-04-09 14:11:25 -0500

    remove unecessary debugging code

--- src/render/2d/kekule.render.renderer2D.js
+++ src/render/2d/kekule.render.renderer2D.js
@@ -548,7 +548,6 @@ Kekule.Render.Base2DRenderer = Class.create(Kekule.Render.CompositeRenderer,  //
 	},
 	drawRichText: function(context, coord, richText, options)  // note: return {drawnObj, boundRect}
 	{
-		var metrics = context.measureText(richText.anchorItem.anchorItem.text);
 		var drawer = this.getRichTextDrawer();
 		// debug
 		//console.log('draw richText', richText, options);
